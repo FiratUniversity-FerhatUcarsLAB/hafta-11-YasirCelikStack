c734942ba46c52c503310fa19236841d76ec62d1
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-12-03 19:00:30 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,5 @@
+//250542010 Muhammet Yasir Çelik
+
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -13,9 +15,11 @@ public class Exercise4_4 {
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
 
+        //Derleyici hata vermez, sadece bazı IDE’ler “Sonuç kullanılmıyor (unused result)” şeklinde uyarı gösterebilir.
+
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
+        //Derleyici hata verir: “void type not allowed here” veya “bad operand types”.
         // Cevaplarinizi yorum olarak ekleyin.
     }
 }
