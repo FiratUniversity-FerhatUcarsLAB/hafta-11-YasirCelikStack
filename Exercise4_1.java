d61e64fa435772c7cd45dcc4f07d218b3c3d609f
250542010 yasir_celik <154225499+YasirCelikStack@users.noreply.github.com>
2025-12-03 18:30:04 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,25 @@
 public class Exercise4_1 {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+    public static void printAmerican(String day, String month, int date, int year) {
+        System.out.println("Amerikan Formati");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println();
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    // Avrupa formatı: 22 July 2019, Monday
+    public static void printEuropean(String day, String month, int date, int year) {
+        System.out.println("Avrupa Formati");
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
-    public static void main(String[] args) {
-        // TODO: test methods
+   public static void main(String[] args) {
+        // Örnek kullanım:
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        printAmerican(day, month, date, year);
+        printEuropean(day, month, date, year);
     }
 }
